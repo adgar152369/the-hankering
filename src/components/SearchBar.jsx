881bc1acf7d5bc611520929cc5b52a3fd82b413d
Adam <adgar1523@gmail.com>
2024-07-06 15:34:02 -0700

    dynamic active sort by button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,19 +2,42 @@ import { useState } from "react";
 import styles from "./SearchBar.module.css";
 
 export default function SearchBar() {
-  const [sortBy, setSortBy] = useState({ sort_by: 'best_match' });
+  const [sortBy, setSortBy] = useState('best_match');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchLocation, setSearchLocation] = useState('');
 
-  function handleSortBy(e) {
-    setSortBy({ sort_by: e.target.value });
+
+  function handleSortBy({ target }) {
+    setSortBy(target.value);
   }
+
+  console.log(sortBy === 'best_match')
 
   return (
     <div className={styles.SearchBar}>
       <div className={styles.SearchBarContent}>
         <div className={styles.SearchBarSortByBtns}>
-          <button onClick={handleSortBy} value='best_match'>Best Match</button>
-          <button onClick={handleSortBy} value='rating'>Highest Rated</button>
-          <button onClick={handleSortBy} value='review_count'>Most Reviewed</button>
+          <button
+            className={
+              `${styles.SearchBarSortByBtn} 
+              ${sortBy === 'best_match' ? styles.SearchBarSortByBtnActive : undefined}`
+            }
+            onClick={handleSortBy}
+            value='best_match'>Best Match</button>
+          <button
+            className={
+              `${styles.SearchBarSortByBtn} 
+              ${sortBy === 'rating' ? styles.SearchBarSortByBtnActive : undefined}`
+            }
+            onClick={handleSortBy}
+            value='rating'>Highest Rated</button>
+          <button
+            className={
+              `${styles.SearchBarSortByBtn} 
+              ${sortBy === 'review_count' ? styles.SearchBarSortByBtnActive : undefined}`
+            }
+            onClick={handleSortBy}
+            value='review_count'>Most Reviewed</button>
         </div>
         <div className={styles.SearchBarInputs}>
           <input className={styles.SearchBarInput} type="text" placeholder="Search Businesses" />
